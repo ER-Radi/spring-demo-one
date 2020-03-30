be8ddbf382855b38b81d66b4b409e63e8197bf83
erradi <mohamed.erradi@nimbleways.com>
2020-03-30 10:47:52 +0200

    Inversion of Control - XML Configuration

---
+++ src/com/lov2code/springdemo/BaseballCoach.java
@@ -0,0 +1,10 @@
+package com.lov2code.springdemo;
+
+public class BaseballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes on batting practice";
+    }
+
+}
