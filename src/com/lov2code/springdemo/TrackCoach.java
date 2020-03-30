1ef39b94bfd60cbb129c8d6f36b839cee18f2072
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2020-03-30 10:58:54 +0200

    Dependency Injection (Constructor Injection) - XML Configuration

--- src/com/lov2code/springdemo/TrackCoach.java
+++ src/com/lov2code/springdemo/TrackCoach.java
@@ -2,9 +2,23 @@ package com.lov2code.springdemo;
 
 public class TrackCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    public TrackCoach() {
+    }
+
+    public TrackCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Run a hard 5Km";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return "Just Do It: " + fortuneService.getFortune();
     }
 
 }
