5fde42261d700c4d155586161950e7f05b4fece2
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2020-03-31 12:55:03 +0200

    Bean Lifecycle Methods

---
+++ src/com/lov2code/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,20 @@
+package com.lov2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+    public static void main(String[] args) {
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+
+        // retrieve bean from spring conatainer
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        System.out.println(theCoach.getDailyWorkout());
+
+        // close the context
+        context.close();
+    }
+}
