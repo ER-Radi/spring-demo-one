7ad1de3cd1a9dc38fff3390becf8acf82c35c1c6
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2020-04-06 10:05:53 +0200

    Bean Scopes (singleton & prototype) with Annotations

---
+++ src/com/lov2code/springdemo_annotations/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,33 @@
+package com.lov2code.springdemo_annotations;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+    public static void main(String[] args) {
+
+        // load spring config file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("resources/springdemo_annotations/applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+
+        // check if they are the same
+        boolean result = (theCoach == alphaCoach);
+
+        // print the results
+        System.out.println("\nPointing the same object: " + result);
+
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+
+        System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+
+        // close the context
+        context.close();
+
+    }
+
+}
