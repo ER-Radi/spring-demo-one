ea1fd217ceef4fbf8a97dc51c7843585370757b8
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2020-04-06 10:55:55 +0200

    Spring Configuration with Java Code [@Configuration & @ComponentScan]

---
+++ src/com/lov2code/springdemo_annotations/SportConfig.java
@@ -0,0 +1,10 @@
+package com.lov2code.springdemo_annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.lov2code.springdemo_annotations")
+public class SportConfig {
+
+}
