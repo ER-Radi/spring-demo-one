fe8b977ad49f21a77e64c64b6916e294728660f9
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2020-04-06 11:33:45 +0200

    Injecting Values from Properties File [@PropertySource & @Value]

--- src/com/lov2code/springdemo_annotations/SportConfig.java
+++ src/com/lov2code/springdemo_annotations/SportConfig.java
@@ -3,9 +3,11 @@ package com.lov2code.springdemo_annotations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("com.lov2code.springdemo_annotations")
+@PropertySource("classpath:resources/springdemo_annotations/sport.properties")
 public class SportConfig {
 
     // define bean for our sad fortune service
